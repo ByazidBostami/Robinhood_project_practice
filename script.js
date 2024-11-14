187d6b9dd5f3ec2031dbaeb8b0e0e03ec39e6ba7
byazid <byazid@Byazids-MacBook-Air.local>
2024-11-14 23:45:36 +0600

    login page build practice project

---
+++ script.js
@@ -0,0 +1,9 @@
+// Password visibility toggle
+// const passwordInput = document.getElementById('password');
+// const togglePassword = document.querySelector('.toggle-password');
+
+// togglePassword.addEventListener('click', function() {
+//     const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+//     passwordInput.setAttribute('type', type);
+//     this.textContent = type === 'password' ? '👁️' : '🙈';  // Change icon based on visibility
+// });
